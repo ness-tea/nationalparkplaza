5d92aa8cbd97447d133d1ea44e35cf5682504fb6
nessa-art <nessenceart@gmail.com>
2019-11-29 18:44:54 -0500

    Using php to implement header for removing redundancy across pages

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="UTF-8" />
-    <!-- This is the webside title -->
-    <title>National Park Plaza</title>
-    <!-- This is the link to the css file -->
-    <link href="css/main.css" type="text/css" rel="stylesheet">
-    <!--- Google fonts --->
-    <link href="https://fonts.googleapis.com/css?family=Forum|Julius+Sans+One|Lateef|Open+Sans&display=swap" rel="stylesheet">
-    <!-- This is the icon shows before the url link -->
-    <link rel="icon" href="assets/header-ico.png">
-    <script type="text/javascript" src="scripts/js/searchPage.js"></script>
-</head>
+<?php include_once 'header.php' ?>
 
 <body>
     <!-- This is the header, which style defined in the css -->
     <div class="header">
-        <ul>
+        <?php include_once
+        <!-- <ul>
             <div class="header-left">
                 <li><a class="active" href="index.php">Search</a></li>
                 <li><a href="review.php">Write Review</a></li>
@@ -28,7 +18,7 @@
                 <li><a href="registration.php">Register</a></li>
             </div>
         </ul>
-        <h1 class="title">NATIONAL PARK PLAZA</h1>
+        <h1 class="title">NATIONAL PARK PLAZA</h1> -->
     </div>
 
     <hr class="divider">
