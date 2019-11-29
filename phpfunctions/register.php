4cda6bd765321aec82fc67562c8ec2bced6b9600
nessa-art <nessenceart@gmail.com>
2019-11-29 16:54:00 -0500

    Some debugging logs

--- phpfunctions/register.php
+++ phpfunctions/register.php
@@ -6,28 +6,24 @@ $username = "truonv1";
 $password = "12345678";
 $dbname = "nationalparkplaza";
 
+// Store user input from registration.php
+$fullname = $_POST['fullname'];
+$birthdate = $_POST['birthday'];
+$email = $_POST['email'];
+$pass = md5($_POST['pass']);  // hash password for security
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     // Prepare SQL and bind parameters for preventing malicious attacks/injections
-    $stmt = $conn->prepare("INSERT INTO Users (fullname, birthdate, email, pass) 
-                            VALUES (:fullname, :birthdate, :email, :pass)");  
-    $stmt->bindParam(':fullname', $fullname);
-    $stmt->bindParam(':birthdate', $birthdate);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':pass', $pass);
-
-    // Insert user input from registration.php
-    $fullname = $_POST['fullname'];
-    $birthdate = $_POST['birthday'];
-    $email = $_POST['email'];
-    $pass = md5($_POST['pass']);  // hash password for security
     
-    if (!$stmt -> exec())
-    {
-        echo "Something in exec() failed";
-    }
+    if (!$stmt = $conn->prepare("INSERT INTO Users (fullname, birthdate, email, pass) 
+                            VALUES (:fullname, :birthdate, :email, :pass)"))  
+                            {
+                                echo "Prepare failed";
+                            }
+    $stmt->execute(array(':fullname'=> $fullname, ':birthdate'=> $birthdate, ':email'=> $email, ':pass'=> $pass));
     
     echo "User added successfully";
 }
