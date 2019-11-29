99e90af8f09f45505ff578efe72f44ca9bfc7de5
nessa-art <nessenceart@gmail.com>
2019-11-29 17:37:11 -0500

    debug logging for siginin.php

--- phpfunctions/signin.php
+++ phpfunctions/signin.php
@@ -19,26 +19,26 @@ try
         // Query we are using to check if the user exists in database
         $query = "SELECT * FROM Users WHERE email = :email AND pw = :pass";
 
-        // Prepared statement
-        $stmnt = $pdo->prepare($query);
-        try {
-            $stmnt->execute(array(':email'=> $email, ':pass'=> $pass));
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+        // // Prepared statement
+        // $stmnt = $pdo->prepare($query);
+        // try {
+        //     $stmnt->execute(array(':email'=> $email, ':pass'=> $pass));
+        // } catch (PDOException $e) {
+        //     echo $e->getMessage();
+        // }
 
-        // For getting data from the query to submitted above.
-        $rows = $stmnt->fetchAll();
+        // // For getting data from the query to submitted above.
+        // $rows = $stmnt->fetchAll();
 
-        // If there is only one user
-        if (count($rows) == 1){
-            // Setting the session to the returned user ID.
-            $_SESSION['fullname'] = $rows[0]['fullname'];
-            // Redirect to table of users.
-            header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
-        } else {
-            header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
-        }
+        // // If there is only one user
+        // if (count($rows) == 1){
+        //     // Setting the session to the returned user ID.
+        //     $_SESSION['fullname'] = $rows[0]['fullname'];
+        //     // Redirect to table of users.
+        //     header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
+        // } else {
+        //     header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
+        // }
 
         echo "Log in successful";
 
