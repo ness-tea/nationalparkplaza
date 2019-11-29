e709a28e83d786274255ebee32e7c3cd305bd437
nessa-art <nessenceart@gmail.com>
2019-11-29 12:56:18 -0500

    Fixing sql syntax error

--- phpfunctions/register.php
+++ phpfunctions/register.php
@@ -16,7 +16,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $query = "INSERT INTO Users (fullname, birthdate, email, password) VALUES ('$fullname', '$birthdate', '$email', '$pass') WHERE NOT EXISTS (SELECT * FROM Users WHERE email = '$email')";  
+    $query = "INSERT INTO Users (fullname, birthdate, email, password) VALUES ('$fullname', '$birthdate', '$email', '$pass')";  
 
     $conn->exec($query);
     
