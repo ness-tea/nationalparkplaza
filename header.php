ab7d7d04d8fd9bd42bb340c8896ee2b82a7632ba
nessa-art <nessenceart@gmail.com>
2019-11-29 18:14:12 -0500

    testing menu

---
+++ header.php
@@ -0,0 +1,15 @@
+<!-- This is the header, which style defined in the css -->
+<div class="header">
+    <ul>
+        <div class="header-left">
+            <li><a class="active" href="index.php">Search</a></li>
+            <li><a href="review.php">Write Review</a></li>
+            <li><a href="submit.php">Submit Park</a></li>
+        </div>
+        <div class="header-right">
+            <li><a href="login.php">Login</a></li>
+            <li><a href="registration.php">Register</a></li>
+        </div>
+    </ul>
+    <h1 class="title">NATIONAL PARK PLAZA</h1>
+</div>
