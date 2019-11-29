dafc9e67d107f416e4ed7b00028b75aa1b2f1568
nessa-art <nessenceart@gmail.com>
2019-11-29 12:22:03 -0500

    Adding register.php sql query

--- phpfunctions/register.php
+++ phpfunctions/register.php
@@ -14,13 +14,13 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    // // $query = "INSERT INTO Users (fullname, birthdate, email, password) 
-    // //           VALUES ('$fullname', '$birthdate', '$email', '$pass') 
-    // //           WHERE NOT EXISTS (SELECT email FROM Users WHERE email = '$email') LIMIT 1";  
+    $sql = "INSERT INTO Users (fullname, birthdate, email, password) 
+            VALUES ('$fullname', '$birthdate', '$email', '$pass') 
+            WHERE NOT EXISTS (SELECT email FROM Users WHERE email = '$email' LIMIT 1)";  
     
-    // $conn->exec($query);
+    $conn->exec($sql);
     
-    echo "Connection successful";
+    echo "User added successfully";
 }
 catch(PDOException $e)
 {
