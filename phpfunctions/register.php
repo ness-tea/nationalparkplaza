0a0fc2feb63095d29c19af193b9437f64e41ca47
nessa-art <nessenceart@gmail.com>
2019-11-29 12:12:33 -0500

    removing variable naming

--- phpfunctions/register.php
+++ phpfunctions/register.php
@@ -3,15 +3,25 @@
 $servername = "nationalparkplazadb.cjpr4ybdu2p3.us-east-2.rds.amazonaws.com";
 $username = "truonv1";
 $password = "12345678";
+$dbname = "nationalparkplaza";
 
-try 
-{
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
+$fullname = $_POST['fullname'];
+$birthdate = $_POST['birthday'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    
+    // // $query = "INSERT INTO Users (fullname, birthdate, email, password) 
+    // //           VALUES ('$fullname', '$birthdate', '$email', '$pass') 
+    // //           WHERE NOT EXISTS (SELECT email FROM Users WHERE email = '$email') LIMIT 1";  
+    
+    // $conn->exec($query);
+    
+    echo "Connection successful";
 }
-
 catch(PDOException $e)
 {
     echo $sql . "<br>" . $e->getMessage();
