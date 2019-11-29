0f684b0930d36c885a0c8f38c9ce5ff90a9a009d
nessa-art <nessenceart@gmail.com>
2019-11-29 18:55:37 -0500

    Adding menu.php and header.php

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
 <!DOCTYPE html>
-<html>
 
-<? include('header.php') ?>
+<?php include('header.php'); ?>
 
 <body>
     <!-- This is the header, which style defined in the css -->
     <div class="header">
-        <?php include_once
-        <!-- <ul>
-            <div class="header-left">
-                <li><a class="active" href="index.php">Search</a></li>
-                <li><a href="review.php">Write Review</a></li>
-                <li><a href="submit.php">Submit Park</a></li>
-            </div>
-            <div class="header-right">
-                <li><a href="login.php">Login</a></li>
-                <li><a href="registration.php">Register</a></li>
-            </div>
-        </ul>
-        <h1 class="title">NATIONAL PARK PLAZA</h1> -->
+        <?php include('menu.php'); ?>
     </div>
 
     <hr class="divider">
