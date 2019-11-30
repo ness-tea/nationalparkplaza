5a0c9b614a3b051331461babb2fdecb08880eaa9
nessa-art <nessenceart@gmail.com>
2019-11-29 23:34:54 -0500

    Adding login features

--- phpfunctions/signin.php
+++ phpfunctions/signin.php
@@ -14,32 +14,24 @@ try
     $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if (isset($email) && isset($pass))
+    // Query we are using to check if the user exists in database
+    $query = "SELECT COUNT(email) FROM Users WHERE email = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->execute(array($email));
+
+    // Check if there is the user's entry in the table - meaning that user does exist
+    if ($stmt->fetchColumn() == 1)
     {
-        // Query we are using to check if the user exists in database
-        $query = "SELECT COUNT(email) FROM Users WHERE email = ?";
-        $stmt = $conn->prepare($query);
-        $stmt->execute(array($email, $pass));
-
-        // Check if there is the user's entry in the table - meaning that user does exist
-        if ($stmt->fetchColumn() == 1)
-        {
-            echo "Log in successful";
-            // // Setting the session to the returned user ID.
-            // $_SESSION['user_id'] = $rows[0]['ID'];
-            
-            // // Redirect to table of users.
-            // header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
-        } else {
-            echo "Login not successful";
-            // header("Location: https://{$_SERVER['HTTP_HOST']}/registration.php");
-        }
-
- 
-
+        echo "Log in successful";
+        // // Setting the session to the returned user ID.
+        // $_SESSION['user_id'] = $rows[0]['ID'];
+        
+        // // Redirect to table of users.
+        // header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
     } else {
-        // This path is dependent on where the root of your documents is located.
-        // For this it is made to point back to the index file if login has failed.
-        header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
+        echo "Login not successful";
+        // header("Location: https://{$_SERVER['HTTP_HOST']}/registration.php");
     }
+
+
 ?>
