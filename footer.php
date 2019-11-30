6df2f5b4bdde4911246f115ab009061f631df2a9
nessa-art <nessenceart@gmail.com>
2019-11-29 20:28:12 -0500

    footer changes

--- footer.php
+++ footer.php
@@ -6,7 +6,9 @@
     <footer>
         <!-- This code makes a contact infor box in the footer section on each pages-->
         <h1 class="footer-title"><b>Contact Information</b></h1>
-        Vanessa Truong <br/>
+        <br/>
+        Vanessa Truong 
+        <br/>
         <a href="mailto:truonv1@mcmaster.ca">truonv1@mcmaster.ca</a>
         </p>
     </footer>
