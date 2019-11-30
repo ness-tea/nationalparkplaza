fa86a6926aa26c312f59b249aef398606e2ed02b
nessa-art <nessenceart@gmail.com>
2019-11-30 03:49:11 -0500

    Checking if user exists in login.php

--- phpfunctions/signin.php
+++ phpfunctions/signin.php
@@ -22,33 +22,25 @@ try {
     $stmt->execute(array($email));
 
     $data = $stmt->fetchAll();
+    print_r($data);
 
-    if (empty($data) || !$data)
-    {
-        echo "Issue with fetchAll";
-    }
-    else
-    {
-        echo "fetchall is fine";
-    }
+    // // // Check if there is the user's entry in the table - meaning that user does exist
+    // if ($stmt->rowCount() == 0 )
+    // {
+    //     // User does not exist
+    //     echo "Login unsuccessful";
 
-    // // Check if there is the user's entry in the table - meaning that user does exist
-    if ($stmt->rowCount() == 0)
-    {
-        // User does not exist
-        echo "Login unsuccessful";
-
-        // Redirect to table of users.
-        header("Location: https://{$_SERVER['HTTP_HOST']}/login.php");
-    } 
-    else 
-    {
-        // User exists
-        echo "Login successful";
-        $_SESSION['user_id'] = $data['user_id'];
+    //     // Redirect to table of users.
+    //     header("Location: https://{$_SERVER['HTTP_HOST']}/login.php");
+    // } 
+    // else 
+    // {
+    //     // User exists
+    //     echo "Login successful";
+    //     $_SESSION['user_id'] = $data['user_id'];
         
-        header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
-    }
+    //     header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
+    // }
 
 }
 catch(PDOException $e)
