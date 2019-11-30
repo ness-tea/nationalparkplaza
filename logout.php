980b71037ef79b9870d73cd09f2ff7032dc44f3f
nessa-art <nessenceart@gmail.com>
2019-11-30 05:36:27 -0500

    Checking if user exists in login.php

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php 
     session_start();
+    session_destroy();
     unset($_SESSION['user_id']);
     unset($_SESSION['loggedin']);
     header("Location: https://{$_SERVER['HTTP_HOST']}/index.php");
