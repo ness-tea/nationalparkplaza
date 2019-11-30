e29b5e5ccab085ba6bd430fefbdcf28473597ef7
nessa-art <nessenceart@gmail.com>
2019-11-29 19:35:50 -0500

    Adjusting footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- This is the section of footer, which style defined in the css -->
+
+<hr class="divider">
+
+<div class="footer">
+    <footer>
+        <!-- This code makes a contact infor box in the footer section on each pages-->
+        <h1 class="footer-title"><b>CONTACT</b></h1>
+        <h3>Vanessa Truong<h3> 
+        <p><a href="mailto:truonv1@mcmaster.ca">truonv1@mcmaster.ca</a></p>
+    </footer>
+</div>
+
+</body>
+
+</html>
