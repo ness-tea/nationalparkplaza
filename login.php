8ec2c5286226d2092e2666511bdf963db9a63bc3
nessa-art <nessenceart@gmail.com>
2019-11-30 11:44:36 -0500

    Formatting profile page

--- login.php
+++ login.php
@@ -6,44 +6,44 @@
     include('menu.php');
 ?>
 
-    <!-- Wrapper for registration page content -->
-    <div class="wrap-login">
-        <!-- Applying indent css styling -->
-        <div id="indent"></div>
+<!-- Wrapper for registration page content -->
+<div class="wrap-login">
+    <!-- Applying indent css styling -->
+    <div id="indent"></div>
 
-        <!-- Registration page content -->
-        <div class="login-content">
-            <h1>Log into an Account</h1>
-            <br/>
-            You must be logged in to write a review or submit a park. 
-            <br/>
-            Log in to explore National Parks in your area today!
-            
-            <!-- Form tag for submitting user name, birth date, email and account password, as well as the submit/reset buttons -->
-            <div class="login-form"  >
+    <!-- Registration page content -->
+    <div class="login-content">
+        <h1>Log into an Account</h1>
+        <br/>
+        You must be logged in to write a review or submit a park. 
+        <br/>
+        Log in to explore National Parks in your area today!
+        
+        <!-- Form tag for submitting user name, birth date, email and account password, as well as the submit/reset buttons -->
+        <div class="login-form"  >
 
-                <!-- Call validateRegistration on submit input -->
-                <form action="phpfunctions/users.php" name="signin" method="post">
-                    <br>
-                    <p>Email:</p>
-                    <input required type="email" id="email" name="email">
+            <!-- Call validateRegistration on submit input -->
+            <form action="phpfunctions/users.php" name="signin" method="post">
+                <br>
+                <p>Email:</p>
+                <input required type="email" id="email" name="email">
 
-                    <br>
-                    <p>Account password:</p>
-                    <input required type="password" id="pass" name="pass">
-                    
-                    <br>
-                    <br>
-                    
-                    <input type="submit" name="btnLogin" value="Log In">
-                    <input type="reset" value="Reset">
+                <br>
+                <p>Account password:</p>
+                <input required type="password" id="pass" name="pass">
+                
+                <br>
+                <br>
+                
+                <input type="submit" name="btnLogin" value="Log In">
+                <input type="reset" value="Reset">
 
-                    <br>
-                    <p style="font-size: 1em">Don't have an account? <a href="registration.php"><b>Register now</b></a></p>
-                </form>
-            </div>
+                <br>
+                <p style="font-size: 1em">Don't have an account? <a href="registration.php"><b>Register now</b></a></p>
+            </form>
         </div>
-        <br/><br/><br/>
     </div>
+    <br/><br/><br/>
+</div>
 
 <?php include('footer.php'); ?>
--- profile.php
+++ profile.php
@@ -5,6 +5,45 @@
     include('menu.php');
 ?>
 
+<!-- Wrapper for registration page content -->
+<div class="wrap-profile">
+    <!-- Applying indentations from css styling -->
+    <div id="indent"></div>
+
+    <!-- Registration page content -->
+    <div class="profile-content">
+        <h1>Your Profile</h1>
+        <br/>
+        <br/>
+        <h3>Name:</h3>
+        <p>Place Holder Name here.</p>
+        <br/>
+        <br/>
+        <h3>Email:</h3>
+        <p>Place Holder Email here.</p>
+        <br/>
+        <br/>
+        <h3>Birth Date:</h3>
+        <p>Place Holder Birth Date here.</p>
+        
+        <!-- Display Reviews by current user -->
+        <h2>Your Reviews</h2>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <h2>Your Parks</h2>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <a href="registration.php"><b>Write another review</b></a>
+        <a href="submit.php"><b>Submit a park</b></a>
+       
+    </div>
+    <br/><br/><br/>
+</div>
+
 <?php
     include('footer.php');
 ?>
