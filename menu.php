37f4c9862ae63340fba26c104559a6ecb27c27a2
nessa-art <nessenceart@gmail.com>
2019-11-30 06:25:51 -0500

    Adding session checks to every page

--- menu.php
+++ menu.php
@@ -28,7 +28,7 @@ $current_page = PageName();
                        href="<?php echo $_SESSION['loggedin'] == true ? 'logout.php':'login.php' ?>">
                        <?php echo $_SESSION['loggedin'] == true ? 'Logout':'Login' ?>
                     </a></li>
-                <?php echo $_SESSION['loggedin'] == true ? NULL:`<li><a class="<?php echo `$current_page` == 'registration.php' ? 'active':NULL ?>" href="registration.php">Register</a></li>` ?>
+                <?php echo $_SESSION['loggedin'] == true ? NULL:`<li><a class="<?php echo $current_page == 'registration.php' ? 'active':NULL ?>" href="registration.php">Register</a></li>` ?>
             </div>
         </ul>
 
