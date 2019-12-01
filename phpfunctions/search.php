4bf669a4ef21751982215e092b4ffcfda378237e
nessa-art <nessenceart@gmail.com>
2019-11-30 23:45:04 -0500

    Styling index.html

--- phpfunctions/search.php
+++ phpfunctions/search.php
@@ -6,8 +6,12 @@ $username = "truonv1";
 $password = "12345678";
 $dbname = "nationalparkplaza";
 
+// Store user input if searched by name
 $search_name = $_POST['name'];
 
+// Store user input if searched by rating
+// Because we are using submit onclick for rating search, store the URL as a string first and
+// extract last char for the rating
 $url = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 $search_rating = substr($url,-1);
 
@@ -16,7 +20,15 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    print_r($search_rating);
+    if (isset($_POST['rating']))
+    {
+        echo "Searching by rating.";
+    }
+    else if (isset($_POST['name']))
+    {
+        echo "Searching by name.";
+    }
+
 }
 catch(PDOException $e)
 {
