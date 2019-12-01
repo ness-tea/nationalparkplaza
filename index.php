ad4d868a477b5a6e5ded76261d8effd0f84090a2
nessa-art <nessenceart@gmail.com>
2019-11-30 22:04:49 -0500

    Adding search.php to dynamically generate results page based on user's park search

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 <!-- This code makes a search box -->
                 <form action="phpfunctions/search.php" method="post">
                     <!-- This code makes show word "Search..." in search box -->
-                    <input type="buttom" placeholder="Search..." name="search">
+                    <input placeholder="Search..." id="search" name="search">
                     <!-- This code makes a submit buttom after search box -->
                 </form>
 
