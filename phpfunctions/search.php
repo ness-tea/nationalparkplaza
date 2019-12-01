18d8cb963507b4b3b2d648de2d45710a45114739
nessa-art <nessenceart@gmail.com>
2019-11-30 23:21:50 -0500

    Styling index.html

--- phpfunctions/search.php
+++ phpfunctions/search.php
@@ -14,7 +14,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Search successful.";
+    print_r($search_rating);
 }
 catch(PDOException $e)
 {
