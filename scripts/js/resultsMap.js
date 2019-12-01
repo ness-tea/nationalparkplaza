04f5d0003c96cc401320ec7b20316aae31e6e061
nessa-art <nessenceart@gmail.com>
2019-12-01 15:53:10 -0500

    Adding reviews to dynamic park page

--- scripts/js/resultsMap.js
+++ scripts/js/resultsMap.js
@@ -7,36 +7,17 @@ var centerCords = {
     lng: -79.870020
 };
 // Marker var list with the parks name, latlongtitude, plcae infor and a link url
-var markersOnMap = [{
-        placeName: "Georgian Bay Islands National Park",
-        LatLng: [{
-            lat: 44.874301,
-            lng: -79.870020
-        }],
-        placeInfo: "Swim in Lake Huron’s clear waters. Cycle wooded island trails. Hike paths that meander between ecosystems. Unwind at a cosy cabin.",
-        placeHref: "object2.html"
-
-    },
-    {
-        placeName: "Point Pelee National Park",
-        LatLng: [{
-            lat: 41.962795,
-            lng: -82.518484
-        }],
-        placeInfo: "Canada’s second smallest but most diverse national park, Point Pelee’s forest hosts diverse habitats that provide a sanctuary for plants and animals rarely found elsewhere in the country and the nature lovers who enjoy it.",
-        placeHref: "object3.html"
-    }
-];
+var markersOnMap = [];
 
 function addMarker(park)
 {
     var marker = {
-        placeName: park[3],
+        placeName: park['parkname'],
         LatLng: [{
-            lat: park[1],
-            lng: park[2]
+            lat: park['latitude'],
+            lng: park['longitude']
         }],
-        placeInfo: "hello",
+        placeInfo: park['dsc'],
         placeHref: "park.php"
     }
 
@@ -65,6 +46,7 @@ function addMarkersOnMap() {
         // The content detais shows when click the mark
         // incluing the park name, short infor and link to individual park page
         var contentString = '<div id="content"><h4>' + markersOnMap[i].placeName +
+            '</h4><p>' + markersOnMap[i].placeInfo + '</p>' +
             '<a href=' + markersOnMap[i].placeHref + '>View More Details</a>' + '</div>';
         // Set google maps marker position
         const marker = new google.maps.Marker({
