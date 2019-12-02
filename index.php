d3a4ef96dd08b54dd5b64e7c9755e1df3ecf87c5
nessa-art <nessenceart@gmail.com>
2019-12-01 20:04:45 -0500

    Adding search by location option

--- index.php
+++ index.php
@@ -43,13 +43,9 @@
             </div>
 
             <div class="search-inline">
-                <p>Search by Location:</p>
+                <p id="find-me">Search by Location:</p>
                 <form action="search.php" method="post">
-                    <button type="submit" id="location" name="location" value="">Find Near Me</button>
-                    <script>
-                        var elem = document.getElementById("location");
-                        elem.value = getLocation();
-                    </script>
+                    <button type="submit" onclick="getLocation()" id="location" name="location" value="">Find Near Me</button>
                 </form>
                 <p id="find-me"></p>
             </div>
